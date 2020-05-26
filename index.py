d5595d8aafec3414744631ba3bac7910db368ca4
yels <yels@cc.cc>
2020-05-26 15:38:50 +0800

    asd

--- index.py
+++ index.py
@@ -1,4 +1,18 @@
-from configobjData.index import Config
 
-t = Config()
-print(t.getConfig())
+class A:
+	
+	def play(ad,b = ''):
+		print(dir(ad))
+		ad.name()
+		print(b)
+
+
+	def name(self):
+		print('本身');
+
+	def age():
+		print('18')
+
+a = A();
+a.play('adasd');
+# a.age();
---
+++ mysql/DB/select.py
@@ -0,0 +1,18 @@
+#!usr/bin/env python
+
+import os
+import sys
+import pymysql
+
+class Selectssss:
+
+	msg = 'asd';
+
+	def __init__(self):
+		pass
+
+
+s = Selectssss();
+print(s.msg)
+print(__name__)
+
